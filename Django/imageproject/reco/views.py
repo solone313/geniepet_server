37f1f00aa574307b1c92cf65c81de516dff10136
cjfrn <cjfrn0443@gmail,com>
2019-11-21 15:32:10 +0900

    수정

--- Django/imageproject/reco/views.py
+++ Django/imageproject/reco/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .models import Feed,Dog,Review,Cart,Order
+from .models import Feed,Dog,Cart,Order,Tip,Review
 from .serializers import FeedSerializer,DogSerializer,ReviewSerializer,CartSerializer,OrderSerializer
 from rest_framework import viewsets
+from rest_framework.response import Response
 from keras.models import load_model
 from PIL import Image
 import numpy as np
@@ -10,9 +11,11 @@ from django.http import HttpResponse, JsonResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import permissions
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-import requests
+import random
+from django.db.models import Avg,F
+from django.core import serializers
+import random
+from rest_framework import status
 # Create your views here.
 
 class FeedViewSet(viewsets.ModelViewSet):
@@ -36,7 +39,33 @@ class CartViewSet(viewsets.ModelViewSet):
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+
+# model = load_model('resnet50_dog_model.h5')
+# graph = tf.get_default_graph()
+
 @csrf_exempt
 def post(request):
-    print(request.body)
-    return JsonResponse('hi')
+    return HttpResponse('hi')
+
+def tip(request):
+    queryset = Tip.objects.all()
+    ran = random.randrange(1,4)-1
+    return HttpResponse(queryset[ran].text) 
+
+@csrf_exempt
+def ranking(request):
+    print(request.POST)
+    avg_list = Feed.objects.filter(review__user_dog = request.POST['user_dog']).annotate(score = Avg('review__rating'))
+    ranking_list = avg_list.order_by('-score')
+    max_id = Feed.objects.order_by('-id')[0].id
+    random_id = random.randint(1, max_id+1)
+    random_object = Feed.objects.filter(id__gte=random_id)
+    result = ranking_list | random_object
+    for i in result:
+        print(i,i.score)
+    post_list = serializers.serialize('python',result,fields=('id','name','price','text','image'))
+    actual_data = [d['fields'] for d in post_list]
+    # and now dump to JSON
+    output = json.dumps(actual_data)
+    return HttpResponse(output, content_type="text/json-comment-filtered")
